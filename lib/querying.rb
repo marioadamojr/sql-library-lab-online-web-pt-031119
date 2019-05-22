bd7d3d7f573efce2ac0dc6a88c5a62fc427cc216
Mario Adamo Jr <marioadamojr@gmail.com>
2019-05-22 01:42:28 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -4,8 +4,7 @@ def select_books_titles_and_years_in_first_series_order_by_year
   JOIN series
   ON books.series_id = series.id
   GROUP BY series
-  ORDER BY book.year
-  "
+  ORDER BY book.year  "
 end
 
 def select_name_and_motto_of_char_with_longest_motto
