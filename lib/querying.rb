edab9f1cad6d3f7c32beff23f5d9a89b60dcea1c
Mario Adamo Jr <marioadamojr@gmail.com>
2019-05-22 02:05:23 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -34,6 +34,7 @@ end
 
 def select_series_title_with_most_human_characters
   "SELECT series.title
+  FROM series
   JOIN characters
   ON characters.series_id = series.id
   GROUP BY characters.series
