7282972fcc36d9b40f8f466bd1e93fd266427752
Mario Adamo Jr <marioadamojr@gmail.com>
2019-05-22 01:43:05 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -3,7 +3,7 @@ def select_books_titles_and_years_in_first_series_order_by_year
   FROM books
   JOIN series
   ON books.series_id = series.id
-  GROUP BY series
+  GROUP BY books.series_id
   ORDER BY books.year"
 end
 
