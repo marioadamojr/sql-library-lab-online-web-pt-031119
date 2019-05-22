cdb069dc703b25762c9767226ff8927fcf530fc9
Mario Adamo Jr <marioadamojr@gmail.com>
2019-05-22 02:07:44 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -37,7 +37,8 @@ def select_series_title_with_most_human_characters
   FROM series
   JOIN characters
   ON series.id = characters.series_id
-  WHERE characters.species = 'human'"
+  WHERE characters.species = 'human'
+  LIMIT 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
